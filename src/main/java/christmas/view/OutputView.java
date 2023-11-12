163bde86593c7ed2106b2d76c6b5f162e326a020
WOONBE <kepull2918@naver.com>
2023-11-12 15:05:43 +0900

    [feat] 할인 전 총주문 금액과 증정 메뉴를 출력하는 로직 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,21 @@
 package christmas.view;
 
+import christmas.service.BadgeService;
+import christmas.service.DiscountService;
+import christmas.utils.Parser;
+import christmas.validation.Validator;
+
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
+
+    Validator validator = new Validator();
+    Parser parser = new Parser();
+
+    DiscountService discountService = new DiscountService();
+
+    BadgeService badgeService = new BadgeService();
 
     public void startMessage(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
@@ -15,14 +28,31 @@ public class OutputView {
 
     //메뉴와 개수 print
     public void printOrderMenu(List<String> list1, int [] list2){
-        System.out.println("<주문 메뉴>");
+        System.out.println("\n<주문 메뉴>");
         for (int i = 0; i < list1.size(); i++){
             String menuName = list1.get(i);
             int menuCount = list2[i];
             System.out.printf("%s %d개\n",menuName,menuCount);
         }
-
     }
+    public void printTotalAmount(List<String> list1, int [] list2){
+        System.out.println("\n<할인 전 총주문 금액>");
+        int totalAmount = validator.getTotalOrderAmount(list1,list2);
+        String decimalTotalAmount = parser.inputMoneyToDecimalFormat(totalAmount);
+        System.out.println(decimalTotalAmount+"원");
+    }
+
+    //리팩토링 필요
+    public void printSouvenir(List<String> list1, int [] list2){
+        System.out.println("\n<증정 메뉴>");
+        if(discountService.souvenirService(list1,list2) != 25000){
+            System.out.println("없음");
+            return;
+        }
+        System.out.println("샴페인 1개");
+    }
+
+
 
 
 }
