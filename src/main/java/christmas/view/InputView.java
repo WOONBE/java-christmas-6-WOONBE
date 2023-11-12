e0b50a36a325a1b85ac81f029e81c7a7088f3718
WOONBE <kepull2918@naver.com>
2023-11-12 13:29:15 +0900

    [feat] 입력할 때 보여질 InputView 생성

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,31 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void startMessage(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void beforeInputDay(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    public void inputVisitDay(){
+        Console.readLine();
+    }
+
+    public void beforeInputMenu(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
+
+    public void inputOrderMenu(){
+        Console.readLine();
+    }
+
+
+
+
+
+}
