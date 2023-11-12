b5d73e3a1f4c727d8c596461a938e37beb428f2e
WOONBE <kepull2918@naver.com>
2023-11-12 15:53:10 +0900

    [feat] 혜택 내역 안의 출력을 담당하는 로직들 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,10 +5,11 @@ import christmas.service.DiscountService;
 import christmas.utils.Parser;
 import christmas.validation.Validator;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
+
+    private static final int NO_DISCOUNT = 0;
 
     Validator validator = new Validator();
     Parser parser = new Parser();
@@ -43,7 +44,7 @@ public class OutputView {
     }
 
     //리팩토링 필요
-    public void printSouvenir(List<String> list1, int [] list2){
+    public void printSouvenirMenu(List<String> list1, int [] list2){
         System.out.println("\n<증정 메뉴>");
         if(discountService.souvenirService(list1,list2) != 25000){
             System.out.println("없음");
@@ -51,10 +52,55 @@ public class OutputView {
         }
         System.out.println("샴페인 1개");
     }
+
+    //모든 혜택 내역 출력
+    public void printAllBenefits(){
+        System.out.println("\n<혜택 내역>");
+
+
+    }
     public void printBadge(List<String> list1, int [] list2){
         System.out.println("\n<12월 이벤트 배지>");
         String badge = badgeService.giveBadge(list1,list2);
         System.out.println(badge);
+    }
+
+    //0인지 판별하는 로직 추가해야함(if로)
+    public void printDdayDiscount(int day){
+        int dDayDiscount = discountService.discountOnDday(day);
+        if(dDayDiscount != NO_DISCOUNT) {
+            String decimalDdayDiscount = parser.inputMoneyToDecimalFormat(dDayDiscount);
+            System.out.println("크리스마스 디데이 할인: -" + decimalDdayDiscount + "원");
+        }
+    }
+    public void printDayOfWeekDiscount(int day, List<String> nameList, int [] countList){
+        int dayOfWeekDiscount = discountService.discountOnDayOfWeek(day,nameList,countList);
+        if(dayOfWeekDiscount != NO_DISCOUNT) {
+            String decimalDayOfWeekDiscount = parser.inputMoneyToDecimalFormat(dayOfWeekDiscount);
+            System.out.println("평일 할인: -" + decimalDayOfWeekDiscount + "원");
+        }
+    }
+    public void printWeekendDiscount(int day, List<String> nameList, int [] countList){
+        int weekendDiscount = discountService.discountOnWeekend(day,nameList,countList);
+        if(weekendDiscount != NO_DISCOUNT) {
+            String decimalWeekendDiscount = parser.inputMoneyToDecimalFormat(weekendDiscount);
+            System.out.println("주말 할인: -" + decimalWeekendDiscount + "원");
+        }
+    }
+    public void printSpecialDayDiscount(int day){
+        int specialDayDiscount = discountService.discountOnSpecialDay(day);
+        if(specialDayDiscount != NO_DISCOUNT) {
+            String decimalSpecialDayDiscount = parser.inputMoneyToDecimalFormat(specialDayDiscount);
+            System.out.println("특별 할인: -" + decimalSpecialDayDiscount + "원");
+        }
+    }
+
+    public void printSouvenirDiscount(List<String> list1, int [] list2){
+        int champagnePrice = discountService.souvenirService(list1,list2);
+        if(champagnePrice != NO_DISCOUNT) {
+            String decimalChampagnePrice = parser.inputMoneyToDecimalFormat(champagnePrice);
+            System.out.println("증정 이벤트: -" + decimalChampagnePrice + "원");
+        }
     }
 
 
