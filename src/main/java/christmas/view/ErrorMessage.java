0c9d531721f31282581455b3f7e323581f4a0aa1
WOONBE <kepull2918@naver.com>
2023-11-12 13:08:48 +0900

    [feat] 예외 처리에 사용할 ErrorMessage 작성

---
+++ src/main/java/christmas/view/ErrorMessage.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+public class ErrorMessage {
+
+    public static void inputDayErrorMessage() {
+        System.out.printf("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.\n");
+    }
+
+
+    public static void inputMenuErrorMessage(){
+        System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.\n");
+    }
+
+
+
+}
