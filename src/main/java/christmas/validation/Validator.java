4d18bcc96d5e3b9deda1d57bff5d986d4a12858b
WOONBE <kepull2918@naver.com>
2023-11-12 13:23:04 +0900

    [feat] 입력받은 메뉴 이름이 enum에 저장된 메뉴이름과 같은지 검증하는 로직 추가

--- src/main/java/christmas/validation/Validator.java
+++ src/main/java/christmas/validation/Validator.java
@@ -3,6 +3,11 @@ package christmas.validation;
 import christmas.domain.Menu;
 import christmas.view.ErrorMessage;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static christmas.domain.Menu.getMenuNames;
+
 public class Validator {
 
     private Menu menu;
@@ -26,6 +31,18 @@ public class Validator {
         }
         return true;
     }
+    //메뉴 검증
+    //1. 없는 메뉴일시
+    public boolean isValidMenuName(List<String> nameList){
+        String[] menuNameList = getMenuNames();
+        for(String name : nameList){
+            if (Arrays.stream(menuNameList).noneMatch(menuName -> menuName.equals(name))){
+                ErrorMessage.inputMenuErrorMessage();
+                throw new IllegalArgumentException();
+            }
+        }return true;
+    }
+
 
 
 
